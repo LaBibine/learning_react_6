fa532a3f2fb5c1efc1c65f876df7902959d11c0a
LaBibine <sabine.moreira95@gmail.com>
2021-06-07 11:56:17 +0200

    learning how to use useState hook with an increment btn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+{/*function Compteur () {
+  const [ count, setCount] = useState(0)
+  const handleClick = function (e) {
+    e.preventDefault()
+    setCount(10)
+  }
+  return <button onClick={handleClick}>Nombre : {count}</button>
+}*/}
+
+{/*function Compteur () {
+  const [ state, setState] = useState({})
+  const handleClick = function (e) {
+    e.preventDefault()
+    setState(state => {
+      return {...state, count: 10}
+    })
+  }
+  return <div onClick={handleClick}>{JSON.stringify(state)}</div>
+}*/}
+
+{/*function Compteur () {
+  const [ count, setCount] = useState(0)
+  const [ count2, setCount2] = useState(0)
+
+  const handleClick = function (e) {
+    e.preventDefault()
+    setCount(count => count + 1)
+  }
+
+  const handleClick2 = function (e) {
+    e.preventDefault()
+    setCount2(count => count + 2)
+  }
+*/}
+
+function useIncrement (initial, step) {
+  const [count, setCount] = useState(initial)
+  const increment = () => {
+    setCount(count => count + step)
+  }
+  return [count, increment]
+}
+
+function Compteur () {
+  const [count, increment] = useIncrement(0, 2)
+
+  return <>
+    <button onClick={increment}> Incrementer {count}</button>
+  </>
+}
+
+
+render (
+  <div>
+    <Compteur />
+  </div>,
   document.getElementById('root')
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//useState ne peut etre dans une condition ou une boucle
